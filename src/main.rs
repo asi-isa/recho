15350119a4eff1a7eefdbedee4f8bc597842faba
asi-isa <ali.inceoglu@protonmail.com>
2023-02-21 18:40:09 +0100

    recho v0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{ArgAction, Parser};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Echoes given arguments
+    #[arg(required(true))]
+    args: Vec<String>,
+
+    /// Do not print new line
+    #[arg(short, default_value_t = true, action = ArgAction::SetFalse)]
+    newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if !args.args.is_empty() {
+        println!("there are some")
+    }
+
+    for arg in args.args.iter() {
+        print!("{arg} ")
+    }
+
+    if args.newline {
+        println!();
+    }
 }
